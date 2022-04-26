e4040e3daa52a90641082cd0c25b02b0f953c90b
jckim <whd4028@gmail.com>
2022-04-26 23:55:58 +0900

    초기셋팅2

---
+++ mbti/src/main/java/com/trip/mbti/config/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.trip.mbti.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+    
+    // @Override
+    // public void addInterceptors(InterceptorRegistry registry) {
+    //     WebMvcConfigurer.super.addInterceptors(registry);
+    // }
+
+    /*
+    정적리소스 경로 설정
+    */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/");
+     }
+
+    // @Override
+    // public void configureViewResolvers(ViewResolverRegistry registry) {
+    //     registry.jsp("/WEB-INF/jsp/", ".jsp");
+    
+    // }
+
+}
---
+++ mbti/src/main/java/com/trip/mbti/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.trip.mbti.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests()
+        .anyRequest().permitAll() //우선 개발기간동안 모든 요청 인가처리
+        ;
+    }
+}
