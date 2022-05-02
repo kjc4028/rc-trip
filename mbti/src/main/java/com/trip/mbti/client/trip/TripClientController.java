52b9c1ff092f13828cef35b424fd91a1cf5d1e5d
jckim <whd4028@gmail.com>
2022-05-02 23:54:53 +0900

    client trip List 데이터 전달

--- mbti/src/main/java/com/trip/mbti/client/trip/TripClientController.java
+++ mbti/src/main/java/com/trip/mbti/client/trip/TripClientController.java
@@ -1,6 +1,9 @@
 package com.trip.mbti.client.trip;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.types.ObjectId;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -9,19 +12,20 @@ import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Controller
 @RequestMapping("/client")
-@RestController
 public class TripClientController {
     
     @GetMapping("/trip")
-    @ResponseBody
-    public String tripList(){
+    public String tripList(Model model){
         RestTemplate restTemp = new RestTemplate();
         String url = "http://localhost:8080/rest/trip";
         HttpHeaders headers = new HttpHeaders();
@@ -37,20 +41,29 @@ public class TripClientController {
             System.out.println(totalObj.get("data"));
             JSONArray dataObj = (JSONArray) jParser.parse(totalObj.get("data").toString());
             
+            //TripClientDto tripDto = new TripClientDto();
+            List<TripClientDto> tripList = new ArrayList<>();
             if(dataObj.size()>0){
                 for (int i = 0; i < dataObj.size(); i++) {
+                    TripClientDto tripDto = new TripClientDto();
                     JSONObject jObj = (JSONObject) dataObj.get(i);
                     System.out.println(jObj.get("_Id"));
                     System.out.println(jObj.get("tripNm"));
                     System.out.println(jObj.get("tripCts"));
                     System.out.println("==================");
+                    tripDto.setTripNm(jObj.get("tripNm").toString());
+                    tripDto.setTripCts(jObj.get("tripCts").toString());
+                    tripDto.setTripId(jObj.get("_Id").toString());
+                    tripList.add(tripDto);
                 }
             }
-
+            model.addAttribute("tripList", tripList);
+            
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        return responseEntity.getBody();
+        //return responseEntity.getBody();
+        return "client/trip/tripList";
     }
 
 }
