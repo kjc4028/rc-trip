9952ab4361676f74f78525f8e79f62a4402efb2f
jckim <whd4028@gmail.com>
2022-05-22 23:48:14 +0900

    test case add

--- mbti/src/test/java/com/trip/mbti/MbtiApplicationTests.java
+++ mbti/src/test/java/com/trip/mbti/MbtiApplicationTests.java
@@ -1,21 +1,36 @@
 package com.trip.mbti;
 
+import static org.junit.Assert.assertTrue;
+
+import com.trip.mbti.rest.trip.TripEntity;
+import com.trip.mbti.rest.trip.TripService;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
 
 
 @SpringBootTest
 class MbtiApplicationTests {
 	
-	// @Autowired
-	// TripService tripService;
+	@Autowired
+	TripService tripService;
 	
 	@Test
 	public void test() {
-		// TripEntity tripEntity = new TripEntity();
-		// tripEntity.setMbti_1("E");
-		// tripService.findSearchTripMbtiPage(tripEntity, 1, 10);
-		System.out.println("test......kjc....");
+		TripEntity tripEntity = new TripEntity();
+		tripEntity.setMbtia("E");
+		tripEntity.setMbtib("N");
+		tripEntity.setMbtic("F");
+		tripEntity.setMbtid("P");
+		Page<TripEntity> tripPage = tripService.findSearchTripMbtiPage(tripEntity, 1, 10);
+		System.out.println(tripPage.getContent());
+		assertTrue(tripEntity.getMbtia().equals(tripPage.getContent().get(0).getMbtia()));
+		assertTrue(tripEntity.getMbtib().equals(tripPage.getContent().get(0).getMbtib()));
+		assertTrue(tripEntity.getMbtic().equals(tripPage.getContent().get(0).getMbtic()));
+		assertTrue(tripEntity.getMbtid().equals(tripPage.getContent().get(0).getMbtid()));
+		
 	}
 
 }
