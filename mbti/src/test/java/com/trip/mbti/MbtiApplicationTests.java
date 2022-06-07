f49679eba4025fa3945007fcf7c4c507454f534e
jckim <whd4028@gmail.com>
2022-06-08 00:21:52 +0900

    test 추가

--- mbti/src/test/java/com/trip/mbti/MbtiApplicationTests.java
+++ mbti/src/test/java/com/trip/mbti/MbtiApplicationTests.java
@@ -2,13 +2,16 @@ package com.trip.mbti;
 
 import static org.junit.Assert.assertTrue;
 
-import com.trip.mbti.rest.trip.TripEntity;
-import com.trip.mbti.rest.trip.TripService;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+
+import com.trip.mbti.rest.trip.TripEntity;
+import com.trip.mbti.rest.trip.TripService;
 
 
 @SpringBootTest
@@ -33,4 +36,34 @@ class MbtiApplicationTests {
 		
 	}
 
+	@Test
+	public void selectTest(){
+		List<TripEntity> list = tripService.findAllTripEntity();
+		int cnt = 0;
+		for(TripEntity trip : list){
+			System.out.println("=====================");
+			System.out.println(trip.get_Id());
+			System.out.println(trip.getTripNm());
+			System.out.println(trip.getTripCts());
+			cnt++;
+		}
+		
+		assertTrue("cnt 비교 ", cnt == list.size());
+	}
+
+	@Test
+	public void deleteTest(){
+		String testid = "6273bd24271c5e4b63b33bde";
+		Optional<TripEntity> tripEntity = tripService.findOneById(testid);
+		tripService.delete(tripEntity.get());
+		
+		if(tripService.findOneById(testid) == null){
+			assertTrue(true);
+		} else {
+			System.out.println(tripService.findOneById(testid));
+
+		}
+
+		
+	}
 }
