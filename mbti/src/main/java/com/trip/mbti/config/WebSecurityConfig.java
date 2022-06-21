c4f55d60acaa6da5a54b43d9da50dfce6ca2ada3
jckim <whd4028@gmail.com>
2022-06-21 17:50:00 +0900

    jwt 관련 셋팅

--- mbti/src/main/java/com/trip/mbti/config/WebSecurityConfig.java
+++ mbti/src/main/java/com/trip/mbti/config/WebSecurityConfig.java
@@ -2,16 +2,36 @@ package com.trip.mbti.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+import com.trip.mbti.rest.user.jwt.JwtAccessDeniedHandler;
+import com.trip.mbti.rest.user.jwt.JwtAuthenticationEntryPoint;
+import com.trip.mbti.rest.user.jwt.JwtSecurityConfig;
+import com.trip.mbti.rest.user.jwt.TokenProvider;
+
+//@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
-    
+    private TokenProvider tokenProvider;
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    public WebSecurityConfig(
+            TokenProvider tokenProvider,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            JwtAccessDeniedHandler jwtAccessDeniedHandler
+    ) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -19,9 +39,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-        .authorizeRequests()
-        .anyRequest().permitAll() //우선 개발기간동안 모든 요청 인가처리
-        ;
+        // http.csrf().disable()
+        // .authorizeRequests()
+        // .anyRequest().permitAll() //우선 개발기간동안 모든 요청 인가처리
+        // ;
+
+        http
+            .csrf().disable()
+            .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/user/signup").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .apply(new JwtSecurityConfig(tokenProvider));                
+
     }
 }
