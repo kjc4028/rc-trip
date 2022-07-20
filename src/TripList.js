975dd2ecb22743526d7f7e1598f31996cc45a68a
jckim <whd4028@gmail.com>
2022-07-20 17:31:42 +0900

    trip list 초기 구현

--- src/TripList.js
+++ src/TripList.js
@@ -3,45 +3,31 @@ import React, { useState, useEffect } from "react";
 
 function TripList() {
     
-    const [tripList, setTripList] = useState(null);
-    console.log(localStorage.getItem("Authorization"));
-    let listAll = [];    
-    // axios.defaults.headers.common['Authorization'] = `Bearer `+localStorage.getItem("Authorization");
-    // axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization");
-    // axios.get('http://localhost:8080/rest/trips')
-    // .then((res) => {
-    //     console.log("res ---");
-    //     console.log(res);
-    //     //setTripList(res.data.data.content);
-    //     console.log(res.data.data.content);
-    //     listAll = res.data.data.content;
-    //     //setTripList(list);
-    // });
-    // console.log("listAll ---");
-    // console.log(listAll);
-    
-    
-    useEffect(() => {
-        const fetchData = async () => {
-        axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization");
-          const response = await axios.get(
-            "http://localhost:8080/rest/trips"
-          );
-          console.log(response.data.data.content);
-          listAll = response.data.data.content;
-          //let rsList = "<ul>"+"<li>"+response.data.data.content._Id+"</li>"+"</ul>"
-          //setTripList(rsList);
-        };
-        console.log("kjc02---");
-        console.log(listAll);
-        fetchData();
-      }, []);
+    const [tripList, setTripList] = useState();
 
-    return(<>
-    <div className="tripList">
-        trip list
-    </div>
-    </>
+    useEffect(() => {
+        getTrips()
+        }, []);
+          
+    const getTrips = async () => {
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem("Authorization");
+    
+    let response = await axios.get('http://localhost:8080/rest/trips');
+    console.log(response);
+    setTripList(response.data.data.content);
+    }          
+          
+    return(
+        <div className="tripList">
+            trip list
+            <ul>
+                {tripList && tripList.map(trip => (
+                    <li key={trip._Id}>
+                    <span>{trip._Id} - {trip.tripNm}</span>
+                    </li>
+                ))}
+        </ul>
+        </div>
     );
 }
 
