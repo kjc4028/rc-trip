fa083082212dcf7a6343f9e62a7f7886b9fb21e9
jckim <whd4028@gmail.com>
2022-07-22 17:41:20 +0900

    application properties >> yml 형식으로 변경 / discovery client 적용

--- mbti/src/main/java/com/trip/mbti/MbtiApplication.java
+++ mbti/src/main/java/com/trip/mbti/MbtiApplication.java
@@ -2,8 +2,10 @@ package com.trip.mbti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MbtiApplication {
 
 	public static void main(String[] args) {
