ef792a5e2c04ce2c03a2373a88f8035e187fcb99
jckim <whd4028@gmail.com>
2022-12-23 17:51:36 +0900

    redis dependencies 설정 1

--- trip-service/mbti/src/main/java/com/trip/mbti/MbtiApplication.java
+++ trip-service/mbti/src/main/java/com/trip/mbti/MbtiApplication.java
@@ -1,17 +1,29 @@
 package com.trip.mbti;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
+@EnableRedisRepositories
 public class MbtiApplication {
+
+	@Value("${profileValue}")
+	String profileValue;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MbtiApplication.class, args);
 	}
 
+	@PostConstruct
+    private void start() {
+        //System.out.println(">>>>>>>>>>><<<><><><><><>>profileValue = " + profileValue);
+    }
 }
